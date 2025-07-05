8096be01623dab525ca5462887f7e07a355acae7
dimyty <dimo_iordanow@abv.bg>
2025-07-05 22:12:10 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// КРИТИЧНА ПОПРАВКА:
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found!');
 
 const root = createRoot(rootElement);
-
 root.render(
   <StrictMode>
     <App />
